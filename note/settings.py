7b8a9f9c748b879d2933c54503a9afd036b0bdaa
Awanish-26 <awanishyadav996@gmail.com>
2024-01-05 18:34:08 +0530

    .

--- note/settings.py
+++ note/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'compressor',
+    'note'
 ]
 
 MIDDLEWARE = [
@@ -71,7 +72,7 @@ DATABASES = {
     }
 }
 
-DATABASES = {}
+# DATABASES = {}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
